269513bd65001e5f0d789cc531757cb1664d2af8
Rishav Gautam <bishnudawadi909@gmail.com>
2024-10-31 19:53:37 +0545

    delete api

--- src/pages/SingleBlog.jsx
+++ src/pages/SingleBlog.jsx
@@ -1,6 +1,26 @@
+import { useEffect, useState } from "react"
 import Navbar from "../components/Navbar"
+import axios from "axios"
+import { useNavigate,useParams } from "react-router-dom"
 
 function SingleBlog(){
+    const navigate = useNavigate()
+    const {id} = useParams()
+    const [blog,setBlog] = useState({})
+    const fetchSingleBlog = async() => {
+        const response = await axios.get("http://localhost:3000/blog/" + id)
+        setBlog(response.data.data)
+    }
+    useEffect(()=>{
+        fetchSingleBlog()
+    },[])
+    const deleteBlog = () =>{
+        const response = axios.delete('http://localhost:3000/blog/'+ id)
+        
+            alert("Deleted Successfully")
+            navigate('/')
+
+    }
     return(
         <>
         <Navbar/>
@@ -9,22 +29,21 @@ function SingleBlog(){
         <div class="flex flex-col md:flex-row -mx-4">
             <div class="md:flex-1 px-4">
                 <div class="h-[460px] rounded-lg bg-gray-300 dark:bg-gray-700 mb-4">
-                    <img class="w-full h-full object-cover" src="https://cdn.pixabay.com/photo/2020/05/22/17/53/mockup-5206355_960_720.jpg" alt="Product Image"/>
+                    <img class="w-full h-full object-cover" src={`http://localhost:3000/${blog.image}`} alt="Product Image"/>
                 </div>
                 <div class="flex -mx-2 mb-4">
                     <div class="w-1/2 px-2">
                         <button class="w-full bg-gray-900 dark:bg-gray-600 text-white py-2 px-4 rounded-full font-bold hover:bg-gray-800 dark:hover:bg-gray-700">Edit</button>
                     </div>
                     <div class="w-1/2 px-2">
-                        <button class="w-full bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-white py-2 px-4 rounded-full font-bold hover:bg-gray-300 dark:hover:bg-gray-600">Delete</button>
+                        <button class="w-full bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-white py-2 px-4 rounded-full font-bold hover:bg-gray-300 dark:hover:bg-gray-600" onClick={deleteBlog}>Delete</button>
                     </div>
                 </div>
             </div>
             <div class="md:flex-1 px-4">
-                <h2 class="text-2xl font-bold text-gray-800 dark:text-white mb-2">Product Name</h2>
+                <h2 class="text-2xl font-bold text-gray-800 dark:text-white mb-2">{blog.title}</h2>
                 <p class="text-gray-600 dark:text-gray-300 text-sm mb-4">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed
-                    ante justo. Integer euismod libero id mauris malesuada tincidunt.
+                   {blog.subtitle}
                 </p>
      
               
@@ -32,11 +51,7 @@ function SingleBlog(){
                 <div>
                     <span class="font-bold text-gray-700 dark:text-gray-300">Product Description:</span>
                     <p class="text-gray-600 dark:text-gray-300 text-sm mt-2">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-                        sed ante justo. Integer euismod libero id mauris malesuada tincidunt. Vivamus commodo nulla ut
-                        lorem rhoncus aliquet. Duis dapibus augue vel ipsum pretium, et venenatis sem blandit. Quisque
-                        ut erat vitae nisi ultrices placerat non eget velit. Integer ornare mi sed ipsum lacinia, non
-                        sagittis mauris blandit. Morbi fermentum libero vel nisl suscipit, nec tincidunt mi consectetur.
+                        {blog.description}
                     </p>
                 </div>
             </div>
